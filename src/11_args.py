a22407f3d835c25a6a4618650e8ed110207c0fce
whitneyxlachelle <whiggins16@yahoo.com>
2020-09-02 14:58:46 -0500

    mvp met

--- src/11_args.py
+++ src/11_args.py
@@ -7,23 +7,22 @@ This is what you'd consider to be a regular, normal function.
 """
 
 
-# def f1(a, b):
-#     return a + b
+def f1(a, b):
+    return a + b
 
 
-#print(f1(1, 2))
+print(f1(1, 2))
 
 # Write a function f2 that takes any number of integer arguments and returns the
 # sum.
 # Note: Google for "python arbitrary arguments" and look for "*args"
 
-
-# def f2(*args):
-#     sum = 0
-#     for x in args:
-#         sum += x
-#     return sum
-
+# abritrary arguments - *args passes a number of arguments
+def f2(*args):
+    sum = 0
+    for x in args:
+        sum += x
+    return sum
 
 # print(f2(1))                    # Should print 1
 # print(f2(1, 3))                 # Should print 4
@@ -34,7 +33,9 @@ This is what you'd consider to be a regular, normal function.
 
 # How do you have to modify the f2 call below to make this work?
 # add * "unpacking operator", it tells print() to unpack the list "a"
+#
 # print(f2(*a))    # Should print 22
+
 
 """
 Write a function f3 that accepts either one or two arguments. If one argument, it returns that value plus 1. 
@@ -44,17 +45,16 @@ Note: Google "python default arguments" for a hint.
 """
 
 
-def f3(num, num2 = 0):
-    if num and num2:
-        return num + num2
-    else:
-        return num + 1
+def f3(num, num2=1):
+
+   return num + num2
 
 print(f3(1, 2))  # Should print 3
 print(f3(8))     # Should print 9
 
 """
-Write a function f4 that accepts an arbitrary number of keyword arguments and prints out the keys and values like so:
+Write a function f4 that accepts an arbitrary number of keyword arguments and
+prints out the keys and values like so:
 
 key: foo, value: bar
 key: baz, value: 12
@@ -62,24 +62,32 @@ key: baz, value: 12
 Note: Google "python keyword arguments".
 """
 
-def f4(par1, par2):
-    print("key: a, value:" + par1)
+"""
+The items() method returns a view object. 
+The view object contains the key-value pairs of a dictionary, 
+as tuples in a list.
+"""
+
+# keyword arguments - I can send arguements with the key = value syntax
+def f4(**kwargs):
+    for key, values in kwargs.items():
+        print(f"key: {key}, value: {values}")
 
 # Should print
 # key: a, value: 12
 # key: b, value: 30
-#f4(a=12, b=30)
+f4(a=12, b=30)
 
 # Should print
 # key: city, value: Berkeley
 # key: population, value: 121240
 # key: founded, value: "March 23, 1868"
-#f4(city="Berkeley", population=121240, founded="March 23, 1868")
+f4(city="Berkeley", population=121240, founded="March 23, 1868")
 
-# d = {
-#     "monster": "goblin",
-#     "hp": 3
-# }
+d = {
+    "monster": "goblin",
+    "hp": 3
+}
 
 # How do you have to modify the f4 call below to make this work?
-# f4(d)
+#f4(d)
--- src/12_scopes.py
+++ src/12_scopes.py
@@ -1,26 +1,30 @@
 # Experiment with scopes in Python.
 # Good reading: https://www.programiz.com/python-programming/global-local-nonlocal-variables
 
+"""
+A local variable can be accessed only locally, a variable defined inside a function is accessible to that function only and can't be used outside the function.
+"""
+
 # When you use a variable in a function, it's local in scope to the function.
 x = 12
 
 def change_x():
-    x = 99
+    global x #declare a global function
+    x = 99 # can now be referenced in the global scope
 
 change_x()
 
 # This prints 12. What do we have to modify in change_x() to get it to print 99?
 print(x)
 
-
 # This nested function has a similar problem.
 
-def outer():
-    y = 120
+def outer(): 
+    y = 120 # variable defines in outer function
 
     def inner():
         y = 999
-
+        print (y) # able to access the variable in the outer function
     inner()
 
     # This prints 120. What do we have to change in inner() to get it to print
